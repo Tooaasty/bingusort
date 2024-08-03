9ee92d8042219a003bac40239a72282b3fbf3ea4
Tooaasty <80686551+Tooaasty@users.noreply.github.com>
2024-08-02 22:52:20 -0400

    Update and rename 2020-11-29.js to 2024-08-02.js

---
+++ src/js/data/2024-08-02.js
@@ -0,0 +1,521 @@
+dataSetVersion = "2024-08-02"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Category",
+    key: "series",
+    tooltip: "Check this to restrict characters.",
+    checked: true,
+    sub: [
+	  { name: "BINGUS Characters", key: "b" },
+          { name: "BINGUS Extras", key: "bx" },
+          { name: "On A Whim Characters", key: "oaw" },
+	  { name: "BFSS Characters", key: "bfss" },
+	  { name: "Comet Comex Characters", key: "cc" },
+	  { name: "Misc Characters", key: "m" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Aglet",
+    img: "w4tqUSC.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Awesome Mail",
+    img: "6gIpRqX.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Awesomer Mail",
+    img: "nVQzBvZ.png",
+    opts: {
+      series: ["bx"]
+    }
+  },
+  {
+    name: "Baggy",
+    img: "SrEm2Hs.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Ball",
+    img: "JoNy1Cs.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Beanstalk",
+    img: "dOzeQ8Q.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Bee",
+    img: "RepqYXM.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Beehive",
+    img: "YffK4Ex.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Bready",
+    img: "6BftVIQ.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Capsule",
+    img: "CaFK3WY.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Cat Tower",
+    img: "eVvHQJb.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Chatty",
+    img: "Fy1rm5U.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Coffee Pot",
+    img: "v67Xy96.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Comet",
+    img: "smAsT05.png",
+    opts: {
+      series: ["cc"]
+    }
+  },
+  {
+    name: "Connie",
+    img: "eJ2iaR1.png",
+    opts: {
+      series: ["cc"]
+    }
+  },
+  {
+    name: "Domino",
+    img: "jBb1qII.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Dude",
+    img: "HXt1He6.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Dyno",
+    img: "yoFG4K1.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Error",
+    img: "Yvxkhdv.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Existy",
+    img: "VilUCx8.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Faily",
+    img: "ITqGh7P.png",
+    opts: {
+      series: ["oaw"]
+    }
+  }, 
+  {
+    name: "Fart in a Jar",
+    img: "xjkNST6.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Flour",
+    img: "OYnwKJh.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Folder",
+    img: "zEucJPS.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Freshly Cut Grass",
+    img: "AkshIum.png",
+    opts: {
+      series: ["bx"]
+    }
+  },
+  {
+    name: "Friend",
+    img: "9vKnFhl.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Glue Stick",
+    img: "UjPemOe.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Googly Eye",
+    img: "XukNkLz.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Graham Cracker",
+    img: "bCluY9S.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Guacamole",
+    img: "HdP4zkK.png",
+    opts: {
+      series: ["m"]
+    }
+  },  
+  {
+    name: "Half-Popped Popcorn",
+    img: "2ck1ErG.png",
+    opts: {
+      series: ["bfss"]
+    }
+  }, 
+  {
+    name: "Hanging Lamp",
+    img: "hakbh0n.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Impostory Jr.",
+    img: "J2kwVq7.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Ladle",
+    img: "tPNlY2Z.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Last Minute",
+    img: "BxvCO1X.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Lava Lamp",
+    img: "SzKsNEc.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+    {
+    name: "Lemon-Lime",
+    img: "JeifHzx.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Line",
+    img: "LpXC8d7.png",
+    opts: {
+      series: ["cc"]
+    }
+  },
+  {
+    name: "Long",
+    img: "SFNQyls.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Lucky Charm",
+    img: "5I6Xliq.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Marble",
+    img: "bMydI5D.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Marinara Sauce",
+    img: "Tbb2xlb.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Mattress",
+    img: "eDpbcO0.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Microwave",
+    img: "Ap26zYd.png",
+    opts: {
+      series: ["bx"]
+    }
+  },
+  {
+    name: "Mini Fridge",
+    img: "cj9WhQq.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Mint Chocolate",
+    img: "ldyKiFx.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Mobile Game",
+    img: "Jfntxyn.png",
+    opts: {
+      series: ["bx"]
+    }
+  },
+  {
+    name: "Mustache",
+    img: "xCg4UZZ.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Noob Head",
+    img: "0dcrJwv.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "North",
+    img: "yGz5FM8.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Orangey",
+    img: "4zmzDor.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Pyramid Steve",
+    img: "Z2ddD8z.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Roasty",
+    img: "KdblhqN.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Shelmet",
+    img: "6KE1v9S.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Shifty",
+    img: "lDD7vK2.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Short",
+    img: "te2wMrY.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Size 5 Brush Point",
+    img: "jkNFuSL.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Sketchy",
+    img: "uKTPAdM.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Slide Whistle",
+    img: "6MEj1a9.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "South",
+    img: "TUH8zqD.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Spoon",
+    img: "DyKSixB.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Sticky",
+    img: "HzMGON5.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Syrup",
+    img: "3679zou.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Thread",
+    img: "O5kMLqX.png",
+    opts: {
+      series: ["bx"]
+    }
+  },
+  {
+    name: "Triangle",
+    img: "YqpLbXp.png",
+    opts: {
+      series: ["oaw"]
+    }
+  },
+  {
+    name: "Tumbleweed",
+    img: "H6B62xY.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Vending Machine",
+    img: "f87BF9c.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Waffle",
+    img: "YXq2IZc.png",
+    opts: {
+      series: ["m"]
+    }
+  },
+  {
+    name: "Wet Floor Sign",
+    img: "xkYnWbJ.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+  {
+    name: "Winrar",
+    img: "Wc8u7dp.png",
+    opts: {
+      series: ["b"]
+    }
+  },
+  {
+    name: "Yield Sign",
+    img: "SNjOb3r.png",
+    opts: {
+      series: ["bfss"]
+    }
+  },
+	
+  
+];
